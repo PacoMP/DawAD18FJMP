772d2f1c29853c0f61669e5538865b72f545b8b3
Francisco Marin Prado <PacoMP@Franciscos-MacBook-Pro-4.local>
2018-08-21 19:42:53 -0500

    added fractalEx

--- DAW/LabsFJMP/Lab4FJMP/lab4-2.js
+++ DAW/LabsFJMP/Lab4FJMP/lab4-2.js
@@ -2,23 +2,28 @@
 
 var array = [0,-2,-3,5,20,34,1,-50,125];
 var matrix = new Array([0,3,4,7,7],[0,6,2,85,34,6],[123,654,65,34,90,98,765,234]);
+var c = document.getElementById("Canvas");
+var ctx = c.getContext("2d");
+ctx.translate(0.5, 0.5);
 
 function ejer1(){
   let boton1 = prompt("Dame un número positivo");
+  if(boton1!=null){
   if(boton1<0){
       alert("El numero es inválido");
-    }else{
-  let table = "<table>";
-  for(let i=1;i<=boton1;i++){
-    table = table + '<tr>';
-    for(let j=1; j<=3;j++){
-      let pow = Math.pow(i,j);
-      table = table+'<td>'+pow+'</td>';
+      }else{
+      let table = "<table>";
+    for(let i=1;i<=boton1;i++){
+      table = table + '<tr>';
+      for(let j=1; j<=3;j++){
+        let pow = Math.pow(i,j);
+        table = table+'<td>'+pow+'</td>';
+      }
+      table = table+('</tr>');
     }
-    table = table+('</tr>');
-   }
-  table = table+'</table>';
-  document.write(table);
+    table = table+'</table>';
+    document.write(table);
+    }
   }
   //div.innerHTML = table;
 }
@@ -32,6 +37,7 @@ function ejer2(){
   msg =msg + rand1 + ' y ';
   msg = msg + rand2 + ' es:';
   let boton = prompt(msg);
+  if(boton !=null){
   let timeF = Date.now()
   let time = (timeF - timeI)/1000;
   let result = rand1 + rand2;
@@ -41,6 +47,7 @@ function ejer2(){
       msg = "El resultado es incorrecto y tardaste "+time+" segundos"
     }
     alert(msg);
+  }
 }
 
 //Contar coincidencias !!!
@@ -101,5 +108,54 @@ function ejer5(number){
   }
   alert("El número invertido es: "+newnum);
 }
+
+
+///Ejercicio6
+function drawLine(p0, p1, color="black") {
+				ctx.beginPath();
+				ctx.moveTo(p0.x, p0.y);
+				ctx.lineTo(p1.x, p1.y);
+				ctx.strokeStyle = color;
+				ctx.lineWidth = 1;
+				ctx.stroke();
+			}
+
+function drawTriangle(p0, p1, p2) {
+		drawLine(p0, p1)
+		drawLine(p1, p2)
+		drawLine(p2, p0)
+	}
+
+      function drawFract(p0, p1, p2, limit){
+      	if(limit > 0){
+      			const pA = {
+      			x: p0.x + (p1.x - p0.x)/2,
+      			y: p0.y - (p0.y - p1.y)/2
+      				},
+      pB = {
+      		x: p1.x + (p2.x - p1.x)/2,
+      		y: p1.y - (p1.y - p2.y)/2
+        		},
+      pC = {
+      		x: p0.x + (p2.x - p0.x)/2,
+      		y: p0.y
+      };
+      drawFract(p0, pA, pC, limit-1);
+      drawFract(pA, p1, pB, limit-1);
+      drawFract(pC, pB, p2, limit-1);
+      }
+      else{
+    drawTriangle(p0,p1,p2);
+  }
+}
+
+
+
+
+
+
+
+
+
 
 //
